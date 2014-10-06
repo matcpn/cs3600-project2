60452c212521736eaa127d5e214e6e4965641923
Alfred Ababio <aababio75@gmail.com>
2014-10-06 16:33:37 -0400

    Added some VCB/Dirent code

--- 3600mkfs.c
+++ 3600mkfs.c
@@ -21,41 +21,97 @@
 #include "3600fs.h"
 #include "disk.h"
 
+#define DE_START 1 // Starts after VCB
+#define DE_LENGTH 100 // Number of file entries
+
+// VCB Data Structure
+typedef struct vcb_s {
+	// Magic number to identify our disk
+	int magic;
+
+	// description of the disk layout
+	int blocksize;
+	int de_start;
+	int de_length;
+	int fat_start;
+	int fat_length;
+	int db_start;
+
+	//metadata for the root directory
+	uid_t user;
+	gid_t group;
+	mode_t mode;
+	struct timespec access_time;
+	struct timespec modify_time;
+	struct timespec create_time;
+} vcb;
+
+// Dirent Data Structure
+typedef struct dirent_s {
+	unsigned int valid;
+	unsigned int first_block;
+	unsigned int size;
+
+	uid_t user;
+	uid_t group;
+	mode_t mode;
+	struct timespec access_time;
+	struct timespec modify_time;
+	struct timespec create_time;
+
+	char name[440]; // Makes the total size 512 bytes
+} dirent;
+
 void myformat(int size) {
-  // Do not touch or move this function
-  dcreate_connect();
+	// Do not touch or move this function
+	dcreate_connect();
+	/*
+	3600: FILL IN CODE HERE.  YOU SHOULD INITIALIZE ANY ON-DISK
+	STRUCTURES TO THEIR INITIAL VALUE, AS YOU ARE FORMATTING
+	A BLANK DISK.  YOUR DISK SHOULD BE size BLOCKS IN SIZE. */
 
-  /* 3600: FILL IN CODE HERE.  YOU SHOULD INITIALIZE ANY ON-DISK
-           STRUCTURES TO THEIR INITIAL VALUE, AS YOU ARE FORMATTING
-           A BLANK DISK.  YOUR DISK SHOULD BE size BLOCKS IN SIZE. */
+	// Set up your vcb
+	vcb myvcb;
+	myvcb.magic = 17;
+	myvcb.blocksize = BLOCKSIZE;
+	myvcb.de_start = DE_START;
+	myvcb.de_length = DE_LENGTH;
+	myvcb.fat_start = (DE_LENGTH + 1);
 
-  /* 3600: AN EXAMPLE OF READING/WRITING TO THE DISK IS BELOW - YOU'LL
-           WANT TO REPLACE THE CODE BELOW WITH SOMETHING MEANINGFUL. */
+	// myvcb.fat_length
+	// myvcb.db_start
 
-  // first, create a zero-ed out array of memory  
-  char *tmp = (char *) malloc(BLOCKSIZE);
-  memset(tmp, 0, BLOCKSIZE);
+	char tmp[BLOCKSIZE];
+	memset(tmp, 0, BLOCKSIZE);
+	memcpy(tmp, &myvcb, sizeof(vcb));
 
-  // now, write that to every block
-  for (int i=0; i<size; i++) 
-    if (dwrite(i, tmp) < 0) 
-      perror("Error while writing to disk");
+	/* 3600: AN EXAMPLE OF READING/WRITING TO THE DISK IS BELOW - YOU'LL
+					 WANT TO REPLACE THE CODE BELOW WITH SOMETHING MEANINGFUL. */
 
-  // voila! we now have a disk containing all zeros
+	// first, create a zero-ed out array of memory  
+	char *tmp = (char *) malloc(BLOCKSIZE);
+	memset(tmp, 0, BLOCKSIZE);
 
-  // Do not touch or move this function
-  dunconnect();
+	// now, write that to every block
+	for (int i=0; i<size; i++) 
+		if (dwrite(i, tmp) < 0) 
+			perror("Error while writing to disk");
+
+	// voila! we now have a disk containing all zeros
+
+	// Do not touch or move this function
+	dunconnect();
 }
 
 int main(int argc, char** argv) {
-  // Do not touch this function
-  if (argc != 2) {
-    printf("Invalid number of arguments \n");
-    printf("usage: %s diskSizeInBlockSize\n", argv[0]);
-    return 1;
-  }
+	// Do not touch this function
+	if (argc != 2) {
+		printf("Invalid number of arguments \n");
+		printf("usage: %s diskSizeInBlockSize\n", argv[0]);
+		return 1;
+	}
 
-  unsigned long size = atoi(argv[1]);
-  printf("Formatting the disk with size %lu \n", size);
-  myformat(size);
+	unsigned long size = atoi(argv[1]);
+	printf("Formatting the disk with size %lu \n", size);
+	myformat(size);
 }
